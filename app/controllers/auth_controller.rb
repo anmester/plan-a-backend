bf4ec89d8b515910dd9c4dd6e8f813e0da2d859c
anmester <github email address>
2019-09-11 17:54:32 -0400

    updated cors

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,7 +4,7 @@ class AuthController < ApplicationController
         @user = User.find_by(username: user_login_params[:username])
         # byebug
         if @user && @user.authenticate(user_login_params[:password])
-            token = JWT.encode({ user_id: @user.id }, 'ice cream')
+            token = JWT.encode({ user_id: @user.id }, ENV['TOKEN_CODE'])
             render json: { user: @user, jwt: token }
         else
             render json: { message: 'Invalid username or password' }
@@ -13,7 +13,7 @@ class AuthController < ApplicationController
 
     def retrieve
         token = request.headers['Authorization']
-        user_id = JWT.decode(token, 'ice cream')[0]["user_id"]
+        user_id = JWT.decode(token, ENV['TOKEN_CODE'])[0]["user_id"]
         user = User.find(user_id)
         render json: user
     end
